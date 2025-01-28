b0e52bf816171fb40f6110e324e8d17877c12a7b
HanTanto <hanitustanto11@gmail.com>
2025-01-28 00:23:33 +0000

    Upload package

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+  name="mypackage",
+  version="0.1",
+  packages=find_packages(),
+  install_requires=[],
+  author="Nama Anda",
+  description="Package contoh dari Colab",
+  python_requires=">=3.6",
+  url="https://github.com/HanTanto/TaskPackageSchool.git"
+)
